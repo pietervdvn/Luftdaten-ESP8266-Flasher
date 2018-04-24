6883888c482d46072aaf09caa9807a35833171ff
Pietervdvn <pietervdvn@gmail.com>
2018-04-24 18:51:37 +0200

    Flasher now gives ID

--- Linux/flash.sh
+++ Linux/flash.sh
@@ -9,6 +9,7 @@ NOCOLOR="$(tput sgr0)"
 USB_DEVICE="$(ls /dev/ttyUSB* 2>/dev/null)"
 FIRMWARE_URL="https://www.madavi.de/sensor/update/data/latest_en.bin"
 FIRMWARE_FILENAME="$(basename $FIRMWARE_URL)"
+IDENTITY_READOUT="IdentityThief.ino.bin"
 
 # Check if esptool is installed
 if command -v esptool 1>/dev/null; then
@@ -28,17 +29,40 @@ else
   echo "USB Device ${GREEN} OK ${NOCOLOR} ${USB_DEVICE}"
 fi
 
-# Replace the firmware binary
-echo "Downloading latest firmware"
-curl --remote-name "$FIRMWARE_URL" --progress-bar
-echo "Firmware   ${GREEN} OK ${NOCOLOR}"
+if [[ -e $FIRMWARE_FILENAME ]]
+then
+	echo "Firmware already exists"
+else
+	echo "Downloading latest firmware"
+	curl --remote-name "$FIRMWARE_URL" --progress-bar
+	echo "Firmware   ${GREEN} OK ${NOCOLOR}"
+fi
+
+
+# esptool -vv -cd nodemcu -cb 921600 -cp /dev/ttyUSB0 -ca 0x00000 -cf
+echo "Flashing identity readout to ${USB_DEVICE}"
+if esptool -vv -cd nodemcu -cb 921600 -ca 0x00000 -cp ${USB_DEVICE} \
+  -cf "$IDENTITY_READOUT"; then
+  echo "Flashing   ${GREEN} OK ${NOCOLOR}"
+else
+  echo "Flashing   ${RED} NOK ${NOCOLOR}"
+  exit
+fi
+
+stty -F ${USB_DEVICE} 115200
+sleep 2
+ESP_ID=`cat ${USB_DEVICE}`
+
+
 
 # Flash the firmware to the ESP8266
 echo "Flashing firmware to ${USB_DEVICE}"
-if esptool -v -cd nodemcu -cb 57600 -ca 0x00000 -cp ${USB_DEVICE} \
+if esptool -vv -cd nodemcu -cb 921600 -ca 0x00000 -cp ${USB_DEVICE} \
   -cf "./$FIRMWARE_FILENAME"; then
   echo "Flashing   ${GREEN} OK ${NOCOLOR}"
 else
   echo "Flashing   ${RED} NOK ${NOCOLOR}"
 fi
+
+echo $ESP_ID
 
